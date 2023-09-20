5b410fb965fdea3c1f128c5a452ac0f434dbaac0
Tahioui <mohamedtahrioui4@gmail.com>
2023-09-21 00:07:25 +0200

    altered turt.py

--- turt.py
+++ turt.py
@@ -6,8 +6,8 @@ def turtlePacket():
     t.forward(80)
     t.right(90)
     t.forward(80)
-    t.left(45)
+    t.left(65)
     t.backward(140)
-for _ in range(1,9):
+for _ in range(1,58):
     turtlePacket()
 window.exitonclick()        
